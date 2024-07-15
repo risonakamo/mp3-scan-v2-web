c0776a77648ae8722bedf3283f64596d9e2a8ce2
risonakamo <hmykanae@gmail.com>
2024-07-15 19:45:17 -0400

    suppress

--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,13 @@ import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 export default {
   preprocess: vitePreprocess(),
+  onwarn(warning,handler)
+  {
+    if (warning.code.includes("a11y"))
+    {
+      return;
+    }
+
+    handler(warning);
+  }
 };
