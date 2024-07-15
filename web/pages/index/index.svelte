1f408c534dfca930ac262bce0d9263427f45a3cd
risonakamo <hmykanae@gmail.com>
2024-07-15 15:56:37 -0400

    more ui stylign

--- web/pages/index/index.svelte
+++ web/pages/index/index.svelte
@@ -8,27 +8,28 @@
 
 <main>
   <div class="info">
-    <p>Name: something.mp3</p>
-    <p>Folder: somewhere</p>
-    <p>Progress: 10 / 21</p>
+    <p>Name: <span class="name">something.mp3</span></p>
+    <p>Folder: <span class="folder">somewhere</span></p>
+    <p>Progress: <span class="progress1">10</span> / <span class="progress2">21</span></p>
+    <p>[=====&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;]</p>
   </div>
 
   <div class="controls">
     <div class="control-container">
       <div class="link-button">
-        > Open Item
+        - Open Item
       </div>
     </div>
 
     <div class="control-container">
-      <h2>Decision:</h2>
-      <div class="link-button indented">YES</div>
-      <div class="link-button indented">NO</div>
-      <div class="link-button indented">MAYBE</div>
+      <div class="link-button">- Next Item</div>
     </div>
 
     <div class="control-container">
-      <div class="link-button">Next Item</div>
+      <h2>Decision:</h2>
+      <div class="link-button indented">- YES</div>
+      <div class="link-button indented">- NO</div>
+      <div class="link-button indented">- MAYBE</div>
     </div>
   </div>
 </main>
