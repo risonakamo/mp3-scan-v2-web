c68a3e5fea9e88e9b8369e54132f6fcd921abec6
risonakamo <hmykanae@gmail.com>
2024-07-15 19:16:21 -0400

    mirroring apis

---
+++ web/apis/mp3-scan-api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const ax=axios.create({
+    baseURL:`http://${window.location.hostname}:4200`,
+});
+
+/** get status from backend */
+export async function getStatus():Promise<Mp3ReviewStatus>
+{
+    return (await ax.get("/get-status")).data;
+}
+
+/** request to open the current item */
+export async function openItem():Promise<void>
+{
+    return (await ax.get("/open-item")).data;
+}
+
+/** request the next item. returns status */
+export async function nextItem():Promise<Mp3ReviewStatus>
+{
+    return (await ax.get("/next-item")).data;
+}
---
+++ web/types/mp3-scan-api.d.ts
@@ -0,0 +1,7 @@
+/** status of backend */
+interface Mp3ReviewStatus
+{
+    currentItem:string
+    totalItems:string
+    currentItemIndex:string
+}
